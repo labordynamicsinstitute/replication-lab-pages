0441e87187143b9d050d82b47e4a13022df2a613
Lars Vilhuber <lars@vilhuber.com>
2019-03-08 17:09:57 -0500

    Create deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Run this script to deploy the app to Github Pages
+
+# Parse cmd arguments
+
+SRC_BRANCH="master"
+DEPLOY_BRANCH="gh-pages"
+
+
+# Exit if any subcommand fails
+set -e
+
+echo "Deploying..."
+echo "Source branch: $SRC_BRANCH"
+echo "Deploy branch: $DEPLOY_BRANCH"
+
+
+#Build site
+#bundle exec jekyll build
+
+# Delete and move files
+#find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name 'CNAME' ! -name '.gitignore' -exec rm -rf {} \;
+cd  _site/
+
+# Push to DEPLOY_BRANCH
+git init
+git add -fA
+git config user.name "${GH_USER_NAME}"
+git config user.email "${GH_USER_EMAIL}"
+git status
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+
+exit 0
